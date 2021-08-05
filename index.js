a7586358c595192ec6a39598c5c72e86d79a1278
Misha <mishawali6@gmail.com>
2021-08-05 15:38:36 -0400

    Viewtasks route and hbs template updated

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { MongoClient } = require("mongodb");
 const app = express()
 const hbs = require('hbs');
-const path    = require('path')
-const bodyParser 		= require('body-parser');
+const path = require('path')
+const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session')
 const passport = require('passport');
 const isLoggedIn = require('./middleware/auth');
@@ -12,7 +12,7 @@ const uri = config.db.uri;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(express.static('views'));
 require('./passport')
-app.set('viewengine','hbs');
+app.set('viewengine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(cookieSession({
   name: 'github-auth-session',
@@ -26,34 +26,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 app.get('/auth/error', (req, res) => res.send('Unknown Error'))
-app.get('/auth/github',passport.authenticate('github',{ scope: [ 'user:email' ] }));
-app.get('/auth/github/callback',passport.authenticate('github', { failureRedirect: '/auth/error' }),
-function(req, res) {
-  res.redirect('/');
-});
+app.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/auth/error' }),
+  function (req, res) {
+    res.redirect('/');
+  });
 
-hbs.registerPartials(__dirname+'/views/partials')
-app.get('/',(req,res,next)=>{
-    let data = {
-        layout: false
-      }
-  res.render('login.hbs',data);
+hbs.registerPartials(__dirname + '/views/partials')
+app.get('/', (req, res, next) => {
+  let data = {
+    layout: false
+  }
+  res.render('login.hbs', data);
 })
-app.get('/github',(req,res)=>{
-    res.redirect('/home');
+app.get('/github', (req, res) => {
+  res.redirect('/home');
 })
-app.get('/home',isLoggedIn,(req,res)=>{
-    res.render('home.hbs');
+app.get('/home', isLoggedIn, (req, res) => {
+  res.render('home.hbs');
 
 })
-app.get('/createtask',isLoggedIn,(req,res)=>{
-    res.render('createtask.hbs');
+app.get('/createtask', isLoggedIn, (req, res) => {
+  res.render('createtask.hbs');
 })
-app.get('/viewtasks',isLoggedIn,(req,res)=>{
-    res.render('viewtasks.hbs');
+app.get('/viewtasks',async(req, res) => {
+  var tasks = await Listtasks();
+  res.render('viewtasks.hbs',{tasks:tasks});
+  
 })
-app.get('/deletetask',isLoggedIn,(req,res)=>{
-    res.render('deletetask.hbs');
+app.get('/deletetask', isLoggedIn, (req, res) => {
+  res.render('deletetask.hbs');
 })
 app.get('/logout', (req, res) => {
   req.session = null;
@@ -61,17 +63,17 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 })
 
-app.post('/createtask',(req,res)=>{
-  var taskname  = req.body.taskname;
-  var desc      = req.body.taskdesc;
-  var course    = req.body.taskcourse;
-  var date      = req.body.date;
-  const record = {name:taskname,description:desc,taskcourse:course,taskdate:date};
+app.post('/createtask', (req, res) => {
+  var taskname = req.body.taskname;
+  var desc = req.body.taskdesc;
+  var course = req.body.taskcourse;
+  var date = req.body.date;
+  const record = { name: taskname, description: desc, taskcourse: course, taskdate: date };
   InsertTask(record).catch(console.dir);
   res.redirect('/viewtasks');
 })
-app.listen(8000,()=>{
-console.log('Serve is up and running at the port 8000')
+app.listen(8000, () => {
+  console.log('Serve is up and running at the port 8000')
 })
 
 async function InsertTask(record) {
@@ -79,9 +81,9 @@ async function InsertTask(record) {
     await client.connect();
 
     const database = client.db("taskdb");
-    const movies = database.collection("tasks");
+    const tasks = database.collection("tasks");
     // create a document to be inserted
-    const result = await movies.insertOne(record);
+    const result = await tasks.insertOne(record);
 
     console.log(
       `${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,
@@ -89,4 +91,18 @@ async function InsertTask(record) {
   } finally {
     await client.close();
   }
+}
+
+async function Listtasks() {
+  try {
+    await client.connect();
+    const database = client.db("taskdb");
+    const data = await database.collection('tasks').find().toArray();
+    return data;
+
+  } finally {
+    await client.close();
+    
+  }
+
 }
