b6c850b44afd83799c5f145f0dd9b7c685d16b62
Misha <mishawali6@gmail.com>
2021-08-05 19:23:08 -0400

    Facebook Auth configured

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session')
 const passport = require('passport');
 const isLoggedIn = require('./middleware/auth');
+const FacebookStrategy = require('passport-facebook').Strategy;
 const config = require('./db/config.json');
 const uri = config.db.uri;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -27,6 +28,13 @@ app.use('/js', express.static(__dirname + '/js'));
 app.use(passport.initialize());
 app.use(passport.session());
 app.get('/auth/error', (req, res) => res.send('Unknown Error'))
+app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect : '/home', failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
 app.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
 app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/auth/error' }),
   function (req, res) {
@@ -43,8 +51,22 @@ app.get('/', (req, res, next) => {
 app.get('/github', (req, res) => {
   res.redirect('/home');
 })
-app.get('/home', isLoggedIn, (req, res) => {
-  res.render('home.hbs');
+app.get('/facebook', (req,res)=>{
+  res.render('fblogin.hbs');
+})
+app.get('/home', isLoggedIn, async(req, res) => {
+  var disname = req.user.displayName;
+  var photo = req.user.photos;
+  var image = photo[0].value;
+  var tasks = await Listtasks();
+  // console.log(tasks);
+  res.render('home.hbs',{userdata:{
+    name:disname,
+    image:image,
+    tasks:tasks
+  }
+
+  });
 
 })
 app.get('/createtask', isLoggedIn, (req, res) => {
@@ -59,8 +81,9 @@ app.get('/edittask', async(req,res)=>{
   var tasks = await Listtasks();
   res.render('edittask.hbs',{tasks:tasks});
 })
-app.get('/deletetask', isLoggedIn, (req, res) => {
-  res.render('deletetask.hbs');
+app.get('/deletetask', async (req, res) => {
+  var tasks = await Listtasks();
+  res.render('deletetask.hbs',{tasks:tasks});
 })
 app.get('/logout', (req, res) => {
   req.session = null;
@@ -75,7 +98,31 @@ app.post('/createtask', (req, res) => {
   var date = req.body.date;
   const record = { name: taskname, description: desc, taskcourse: course, taskdate: date };
   InsertTask(record).catch(console.dir);
+  res.redirect('/home');
+})
+
+app.post('/edittask', async(req,res)=>{
+  var taskid = req.body.taskid;
+  var taskname = req.body.taskname;
+  var desc = req.body.taskdesc;
+  var course = req.body.taskcourse;
+  var date = req.body.taskdate;
+  var o_id = new mongo.ObjectId(taskid);
+  const filter = {'_id': o_id};
+  const options = { upsert: false };
+
+  const record = { name: taskname, description: desc, taskcourse: course, taskdate: date };
+  Updatetask(record,filter,options).catch(console.dir)
   res.redirect('/viewtasks');
+})
+
+app.post('/deletetask', async(req,res)=>{
+  var taskid = req.body.taskid;
+  var o_id = new mongo.ObjectId(taskid);
+  var op = await DeleteTask(o_id);
+  if (op==true) {
+    res.redirect('./viewtasks');    
+  }
 })
 
 app.post('/gettaskdetail',async(req,res)=>{
@@ -123,6 +170,37 @@ async function GettaskDetail(taskid){
     var o_id = new mongo.ObjectId(taskid);
     const data = await database.collection("tasks").findOne({'_id': o_id});
    return data;
+
+  }finally{
+    await client.close();
+  }
+}
+
+async function Updatetask(record,filter,options){
+  try{
+    await client.connect();
+    const database = client.db("taskdb");
+    const tasks = database.collection("tasks");
+    const result = await tasks.replaceOne(filter, record, options);
+    console.log(
+      `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+    );
+  }finally{
+    await client.close();
+    
+  }  
+}
+
+async function DeleteTask(taskid){
+  try{
+    await client.connect();
+    const database = client.db("taskdb");
+    const tasks = database.collection("tasks");
+    const result = await tasks.deleteOne({'_id': taskid});
+    // console.log(
+    //   `${result.matchedCount} document(s) matched the filter, deleted ${result.modifiedCount} document(s)`,
+    // );
+    return true;
 
   }finally{
     await client.close();
